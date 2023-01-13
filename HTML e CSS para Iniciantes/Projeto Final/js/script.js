d51343abe27737479cb8fa8a0942cfa33acf2123
Larissa Narciso <larisnarciso@gmail.com>
2023-01-13 12:34:03 -0300

    js-na-galeria-de-bicicletas

--- HTML e CSS para Iniciantes/Projeto Final/js/script.js
+++ HTML e CSS para Iniciantes/Projeto Final/js/script.js
@@ -43,3 +43,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = window.matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  console.log(media);
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
