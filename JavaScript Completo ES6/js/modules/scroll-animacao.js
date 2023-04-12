3e888d0106fc71161ae198ef3f1396a6e67fb2c4
Larissa Narciso <larisnarciso@gmail.com>
2023-04-12 15:21:25 -0300

    dividindo em módulos

---
+++ JavaScript Completo ES6/js/modules/scroll-animacao.js
@@ -0,0 +1,20 @@
+// Animação Scroll
+export default function initAnimacaoScroll() {
+  const sections = document.querySelectorAll("[data-anime='scroll']");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
---
+++ JavaScript Completo ES6/js/modules/scroll-suave.js
@@ -0,0 +1,28 @@
+// Scroll Link Interno
+export default function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(
+    "[data-menu='suave'] a[href^='#']"
+  );
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    // -- forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: "smooth",
+    // });
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
