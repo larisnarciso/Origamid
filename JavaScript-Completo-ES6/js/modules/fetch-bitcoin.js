3eb2e3e81e67e3ccc4c8ccb205fcfb76c8d1ffc9
Larissa Narciso <larisnarciso@gmail.com>
2023-04-22 15:45:08 -0300

    Refatorar: Fetch Bitcoin

--- JavaScript-Completo-ES6/js/modules/fetch-bitcoin.js
+++ JavaScript-Completo-ES6/js/modules/fetch-bitcoin.js
@@ -1,9 +1,9 @@
 // // alternativa:
-// export default function initFetchBitcoin() {
-//   fetch("https://blockchain.info/ticker")
+// export default function fetchBitcoin(url, target) {
+//   fetch(url)
 //     .then((response) => response.json())
 //     .then((bitcoin) => {
-//       const btcPreco = document.querySelector(".btc-preco");
+//       const btcPreco = document.querySelector(target);
 //       btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
 //     })
 //     .catch((erro) => {
@@ -11,17 +11,13 @@
 //     });
 // }
 
-export default function initFetchBitcoin() {
-  async function fetchBitcoin(url) {
-    try {
-      const btcResponse = await fetch(url);
-      const btcJSON = await btcResponse.json();
-      const btcPreco = document.querySelector('.btc-preco');
-      btcPreco.innerText = (1000 / btcJSON.BRL.sell).toFixed(4);
-    } catch (erro) {
-      console.log(erro);
-    }
+export default async function fetchBitcoin(url, target) {
+  try {
+    const btcResponse = await fetch(url);
+    const btcJSON = await btcResponse.json();
+    const btcPreco = document.querySelector(target);
+    btcPreco.innerText = (1000 / btcJSON.BRL.sell).toFixed(4);
+  } catch (erro) {
+    console.log(erro);
   }
-
-  fetchBitcoin('https://blockchain.info/ticker');
 }
