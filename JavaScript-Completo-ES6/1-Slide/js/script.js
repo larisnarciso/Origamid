d6dad53f887f48ccd121a1180aadb03fc2cca7a7
Larissa Narciso <larisnarciso@gmail.com>
2023-04-23 18:23:56 -0300

    final da navegacao  prev e nav

--- JavaScript-Completo-ES6/1-Slide/js/script.js
+++ JavaScript-Completo-ES6/1-Slide/js/script.js
@@ -2,3 +2,6 @@ import Slide from './slide.js';
 
 const slide = new Slide('.slide', '.slide-wrapper');
 slide.init();
+slide.changeSlide(3);
+// slide.activeNextSlide();
+// slide.activePrevSlide();
