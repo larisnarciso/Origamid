22f493d96dafbc5384520b7ca459cae1f8ca9528
Larissa Narciso <larisnarciso@gmail.com>
2023-08-08 00:24:26 -0300

    componente radio

--- React-Completo/1-Form/src/App.jsx
+++ React-Completo/1-Form/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Input from './Form/Input';
 import Select from './Form/Select';
+import Radio from './Form/Radio';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
 
   return (
     <form>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
       <Select
         options={['Smartphone', 'Tablet']}
         value={produto}
