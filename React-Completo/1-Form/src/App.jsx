e2805b7951c1bbd1b0b32859e345b677ab6a057f
Larissa Narciso <larisnarciso@gmail.com>
2023-08-08 00:33:37 -0300

    componente checkbox

--- React-Completo/1-Form/src/App.jsx
+++ React-Completo/1-Form/src/App.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import Input from './Form/Input';
 import Select from './Form/Select';
 import Radio from './Form/Radio';
+import Checkbox from './Form/Checkbox';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
   const [cor, setCor] = React.useState('');
+  const [linguagens, setLinguagens] = React.useState([]);
 
   return (
     <form>
+      <Checkbox
+        options={['JavaScript', 'PHP', 'Ruby']}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
       <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
       <Select
         options={['Smartphone', 'Tablet']}
