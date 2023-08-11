a61265b3fa712c6248efdded172949f034d29b54
Larissa Narciso <larisnarciso@gmail.com>
2023-08-11 14:30:23 -0300

    css

--- React-Completo/3-Router/Desafio/src/Components/Produto.jsx
+++ React-Completo/3-Router/Desafio/src/Components/Produto.jsx
@@ -26,7 +26,7 @@ const Produto = () => {
     fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
 
-  if (loading) return <div>Carregando...</div>;
+  if (loading) return <div className='loading'></div>;
   if (error) return <p>{error}</p>;
   if (produto === null) return null;
   return (
@@ -35,13 +35,16 @@ const Produto = () => {
         title={`Ranek | ${produto.nome}`}
         description={`Ranek | Esse é um produto: ${produto.nome}`}
       />
-      {produto.fotos.map((foto) => (
-        <img key={foto.src} src={foto.src} alt={foto.titulo} />
-      ))}
+      <div>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
+
       <div>
         <h1>{produto.nome}</h1>
-        <span className='styles.preco'>R$ {produto.preco}</span>
-        <p className='styles.descricao'>{produto.descricao}</p>
+        <span className={styles.preco}>R$ {produto.preco}</span>
+        <p className={styles.descricao}>{produto.descricao}</p>
       </div>
     </section>
   );
