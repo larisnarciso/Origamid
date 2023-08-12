20b233fd8f7e354e8554003c9cf9f5ff98a83d7f
Larissa Narciso <larisnarciso@gmail.com>
2023-08-12 20:31:10 -0300

    estrutura inicial

---
+++ React-Completo/Projeto-Final/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>Todos os direitos reservados</footer>
+  );
+};
+
+export default Footer;
