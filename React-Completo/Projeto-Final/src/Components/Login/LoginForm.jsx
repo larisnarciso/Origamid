b626cee5e40bfcb46cf6b2a04ad60b0e5c5b5284
Larissa Narciso <larisnarciso@gmail.com>
2023-08-16 16:37:14 -0300

    Login Form - CSS

--- React-Completo/Projeto-Final/src/Components/Login/LoginForm.jsx
+++ React-Completo/Projeto-Final/src/Components/Login/LoginForm.jsx
@@ -5,6 +5,8 @@ import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import Error from '../Helper/Error';
 import { UserContext } from '../../UserContext';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Forms/Button.module.css';
 
 const LoginForm = () => {
   const username = useForm();
@@ -22,7 +24,7 @@ const LoginForm = () => {
   return (
     <section className='animeLeft'>
       <h1 className='title'>Login</h1>
-      <form action='' onSubmit={handleSubmit}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label='Usuário' type='text' name='username' {...username} />
         <Input label='Senha' type='password' name='password' {...password} />
         {loading ? (
@@ -32,7 +34,16 @@ const LoginForm = () => {
         )}
         <Error error={error} />
       </form>
-      <Link to='/login/criar'>Cadastro</Link>
+      <Link className={styles.perdeu} to='/login/perdeu'>
+        Perdeu a Senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to='/login/criar'>
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 };
