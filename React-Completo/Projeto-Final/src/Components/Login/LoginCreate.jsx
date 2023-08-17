fbc71dfb0049468fd934a3b02c16a29c8c7d6c75
Larissa Narciso <larisnarciso@gmail.com>
2023-08-17 17:42:14 -0300

    Login Criar - Página de criar novo usuario

--- React-Completo/Projeto-Final/src/Components/Login/LoginCreate.jsx
+++ React-Completo/Projeto-Final/src/Components/Login/LoginCreate.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import useForm from '../../Hooks/useForm';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import { USER_POST } from '../../api';
+import { UserContext } from '../../UserContext';
 
 const LoginCreate = () => {
-  return <div>Login Criar</div>;
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+
+    /* Loga automaticamente apos criar a conta */
+    if (response.ok) {
+      userLogin(username.value, password.value);
+    }
+    console.log(response);
+  }
+
+  return (
+    <section className='animeLeft'>
+      <h1 className='title'>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label='Usuario' type='text' name='username' {...username} />
+        <Input label='Email' type='email' name='email' {...email} />
+        <Input label='Senha' type='password' name='password' {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
--- React-Completo/Projeto-Final/src/Hooks/useForm.jsx
+++ React-Completo/Projeto-Final/src/Hooks/useForm.jsx
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    message:
+      'A senha deve conter pelo menos 8 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula e um número.',
+  },
 };
 
 const useForm = (type) => {
