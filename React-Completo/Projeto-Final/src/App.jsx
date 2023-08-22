8e14a36266aeda88c43e367e13d5eac640873518
Larissa Narciso <larisnarciso@gmail.com>
2023-08-22 17:50:34 -0300

    pagina do perfil do usuario

--- React-Completo/Projeto-Final/src/App.jsx
+++ React-Completo/Projeto-Final/src/App.jsx
@@ -9,6 +9,7 @@ import Footer from './Components/Footer';
 import { UserStorage } from './UserContext';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
               }
             />
             <Route path='foto/:id' element={<Photo />} />
+            <Route path='perfil/:user' element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
---
+++ React-Completo/Projeto-Final/src/Components/User/UserProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Feed from '../Feed/Feed';
+import { useParams } from 'react-router-dom';
+
+const UserProfile = () => {
+  const { user } = useParams();
+  return (
+    <section className='container mainContainer'>
+      <h1 className='title'>{user}</h1>
+      <Feed user={user} />
+    </section>
+  );
+};
+
+export default UserProfile;
