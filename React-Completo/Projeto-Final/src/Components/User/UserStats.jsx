facc7bb3c51fefd758df24fa572c562309427e84
Larissa Narciso <larisnarciso@gmail.com>
2023-08-22 20:00:38 -0300

    utilizando lazy para evitar que a biblioteca externa seja sempre carregada

--- React-Completo/Projeto-Final/src/Components/User/UserStats.jsx
+++ React-Completo/Projeto-Final/src/Components/User/UserStats.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import UserStatsGraphs from './UserStatsGraphs';
 import Head from '../Helper/Head';
 import Loading from '../Helper/Loading';
 import Error from '../Helper/Error';
 import useFetch from '../../Hooks/useFetch';
 import { STATS_GET } from '../../Api';
+
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -21,10 +22,10 @@ const UserStats = () => {
   if (error) return <Error error={error} />;
   if (data) {
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title='Estatísticas' />
         <UserStatsGraphs data={data} />
-      </div>
+      </React.Suspense>
     );
   } else return null;
 };
