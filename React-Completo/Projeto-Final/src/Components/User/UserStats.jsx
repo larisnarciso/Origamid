076c344e50e8ccdd55d1849522f10a606f3603e3
Larissa Narciso <larisnarciso@gmail.com>
2023-08-22 19:36:01 -0300

    total de acesso em estatisticas

--- React-Completo/Projeto-Final/src/Components/User/UserStats.jsx
+++ React-Completo/Projeto-Final/src/Components/User/UserStats.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import UserStatsGraphs from './UserStatsGraphs';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../Api';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title='Estatísticas' />
-      Estatística
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data) {
+    return (
+      <div>
+        <Head title='Estatísticas' />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  } else return null;
 };
 
 export default UserStats;
---
+++ React-Completo/Projeto-Final/src/Components/User/UserStatsGraphs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './UserStatsGraphs.module.css';
+
+const UserStatsGraphs = ({ data }) => {
+  const [graph, setGraph] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
+
+  React.useEffect(() => {
+    setTotal(
+      data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b, 0)
+    );
+  }, [data]);
+
+  return (
+    <section className={`${'animeLeft'} ${styles.graphs}`}>
+      <div className={styles.total}>
+        <p>Acessos: {total}</p>
+      </div>
+    </section>
+  );
+};
+
+export default UserStatsGraphs;
