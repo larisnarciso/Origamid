514ab977bc142170c590e00b9800f522333edb29
Larissa Narciso <larisnarciso@gmail.com>
2023-08-22 17:54:32 -0300

    Criando rota para Error 404 Not Found

--- React-Completo/Projeto-Final/src/App.jsx
+++ React-Completo/Projeto-Final/src/App.jsx
@@ -10,6 +10,7 @@ import { UserStorage } from './UserContext';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
 import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
             />
             <Route path='foto/:id' element={<Photo />} />
             <Route path='perfil/:user' element={<UserProfile />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
--- React-Completo/Projeto-Final/src/Components/User/User.jsx
+++ React-Completo/Projeto-Final/src/Components/User/User.jsx
@@ -5,6 +5,7 @@ import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 import { Routes, Route } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import NotFound from '../NotFound';
 
 const User = () => {
   const { data } = React.useContext(UserContext);
@@ -15,6 +16,7 @@ const User = () => {
         <Route path='/' element={<Feed user={data.id} />} />
         <Route path='postar' element={<UserPhotoPost />} />
         <Route path='estatisticas' element={<UserStats />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </section>
   );
